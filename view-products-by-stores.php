76c2667d0465c4030d76c2436b0c1cf6fbb12859
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-11 03:06:47 -0600

    Update view-products-by-stores.php

--- view-products-by-stores.php
+++ view-products-by-stores.php
@@ -1,1 +1,27 @@
-
+<h1>Products by Store</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Unit Price</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($product = $products->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $product['product_id']; ?></td>
+    <td><?php echo $product['product_name']; ?></td>
+    <td><?php echo $product['product_description']; ?></td>
+    <td><?php echo $product['unit_price']; ?></td>
+  </tr>    
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
