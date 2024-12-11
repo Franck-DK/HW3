d3675f0c8dafe8af9c7456ff542bdda2a590a8fa
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-11 02:13:50 -0600

    Update view-stores.php

--- view-stores.php
+++ view-stores.php
@@ -6,6 +6,7 @@
       <th>ID</th>
       <th>Name</th>
       <th>Location</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -16,6 +17,7 @@ while ($store = $stores->fetch_assoc()) {
     <td><?php echo $store['store_id']; ?></td>
     <td><?php echo $store['store_name']; ?></td>
     <td><?php echo $store['location']; ?></td>
+    <td><a href="products-by-stores.php?id=<?php echo $store['store_id']; ?>">Products</a></td>
   </tr>    
 <?php
 }
