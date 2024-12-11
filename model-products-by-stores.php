66e21e0b4eed787841588582a7318979bffe316f
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-11 03:07:52 -0600

    Update model-products-by-stores.php

--- model-products-by-stores.php
+++ model-products-by-stores.php
@@ -2,7 +2,7 @@
 function selectProductsByStores($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT * FROM products p INNER JOIN orders o ON p.product_id = o.product_id WHERE o.store_id=?;");
+        $stmt = $conn->prepare("SELECT distinct p.product_id, p.product_name, p.product_description, p.unit_price FROM products p JOIN orders o ON p.product_id = o.product_id WHERE o.store_id=?;");
         $stmt->bind_param("i", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
