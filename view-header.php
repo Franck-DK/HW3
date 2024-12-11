d5d1a11aee41a64d20d041af51f0e856f63dd5a5
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-11 00:05:44 -0600

    Create view-header.php

---
+++ view-header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?=$pageTitle?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class = "container">
+
+<nav class="navbar bg-primary" data-bs-theme="dark">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="/">Home</a>
+        <a class="nav-link" href="post.php">Post</a>
+        <a class="nav-link" href="post-result.php">Post Result</a>
+        <a class="nav-link" href="get.php">Get</a>
+        <a class="nav-link" href="get-result.php">Get Result</a> 
+      </div>
+    </div>
+  </div>
+</nav>
+</nav>
+
+      
