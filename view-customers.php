2b230c7d42e248a2f273d07b1f32724e7035b525
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 15:01:39 -0600

    Create view-customers.php

---
+++ view-customers.php
@@ -0,0 +1,29 @@
+<h1>Customers</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Phone</th>
+      <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+<?php
+while ($customer = $customers->fetch_assoc()) {
+?>
+  <tr>
+    <td><?php echo $customer['customer_id']; ?></td>
+    <td><?php echo $customer['customer_firstname']; ?></td>
+    <td><?php echo $customer['customer_lastname']; ?></td>
+    <td><?php echo $customer['phone']; ?></td>
+    <td><?php echo $customer['email']; ?></td>
+  </tr>    
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
