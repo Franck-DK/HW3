6ac8ea9179aa6c7aa069448522d7b64362ea8d10
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 15:19:06 -0600

    Update orders-by-customers.php

--- orders-by-customers.php
+++ orders-by-customers.php
@@ -4,7 +4,7 @@ require_once("model-orders-by-customers.php");
 
 $pageTitle= "Orders by Customers";
 include "view-header.php";
-$orders = selectOrdersByCustomers($_POST['oid']);
+$orders = selectOrdersByCustomers($_POST['cid']);
 include "view-orders-by-customers.php";
 include "view-footer.php";
 ?>
