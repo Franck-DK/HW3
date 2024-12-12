43e7774b8bb7b97eaeedd62fa3a166b1117a646a
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 14:20:43 -0600

    Create orders-by-stores.php

---
+++ orders-by-stores.php
@@ -0,0 +1,16 @@
+<?php
+function selectProductsByStores($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT distinct p.product_id, p.product_name, p.product_description, p.unit_price, o.store_id FROM products p JOIN orders o ON p.product_id = o.product_id WHERE o.store_id=?");
+        $stmt->bind_param("i", $sid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
