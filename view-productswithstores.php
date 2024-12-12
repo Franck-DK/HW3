e4c4e5a2ad4c6668a57a981909632957d49e47f8
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 16:27:16 -0600

    Create view-productswithstores.php

---
+++ view-productswithstores.php
@@ -0,0 +1,32 @@
+<h1>Products with Stores</h1>
+
+<div class="card-group">
+ 
+<?php
+while ($product = $products->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
+      <p class="card-text">
+  <ul class="list-group">
+        <?php
+          $stores = selectStoresByProducts($product['product_id']);
+  while ($store = $stores->fetch_assoc()) {
+    ?>
+    <li class="list-group-item"><?php echo $store['store_id']; ?></li>
+    <li class="list-group-item"><?php echo $store['store_name']; ?></li>
+    <li class="list-group-item"><?php echo $store['location']; ?></li>
+    <li class="list-group-item"><?php echo $store['total_quantity']; ?></li>
+    <?php
+  }
+        ?>
+  </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">Description: <?php echo $product['product_description']; ?></small></p>
+    </div>
+  </div>
+<?php
+}
+</div>
+?>
