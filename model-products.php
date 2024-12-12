382b98754572a8fd1a4956a8b2c08daeda7a649f
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 17:44:47 -0600

    Update model-products.php

--- model-products.php
+++ model-products.php
@@ -12,4 +12,48 @@ function selectProducts() {
         throw $e;
     }
 }
+
+function insertProducts($pname, $pdesc, $pprice) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `products`(`product_name`, `product_description`, `unit_price`) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssd", $pname, $pdesc, $pprice);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateProducts($pname, $pdesc, $pprice, $pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `products` SET`product_name`= ?,`product_description`= ?,`unit_price`= ? WHERE product_id = ?");
+        $stmt->bind_param("ssdi", $pname, $pdesc, $pprice, $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteProducts($pid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `products` WHERE product_id = ?");
+        $stmt->bind_param("i", $pid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
