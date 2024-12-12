cc91f36e29fb497b7b3b63add3727664d7384864
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 15:17:23 -0600

    Update customers.php

--- customers.php
+++ customers.php
@@ -2,7 +2,7 @@
 require_once("util-db.php");
 require_once("model-customers.php");
 
-$pageTitle= "Customerss";
+$pageTitle= "Customers";
 include "view-header.php";
 $customers = selectCustomers();
 include "view-customers.php";
