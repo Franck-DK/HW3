44dea5d2add801c9e902dd2a9595f7bb9cdfaddc
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 16:11:43 -0600

    Update model-productswithstores.php

--- model-productswithstores.php
+++ model-productswithstores.php
@@ -13,10 +13,11 @@ function selectProducts() {
     }
 }
 
-function selectProductsByStores($sid) {
+function selectStoresByProducts($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT distinct p.product_id, p.product_name, p.product_description, p.unit_price, o.store_id FROM products p JOIN orders o ON p.product_id = o.product_id WHERE o.store_id=?");
+        $stmt = $conn->prepare("SELECT s.store_id, s.store_name, s.location, SUM(o.quantity) AS total_quantity FROM stores s JOIN orders o ON s.store_id = o.store_id WHERE o.product_id=?
+GROUP BY s.store_id, s.store_name, s.location");
         $stmt->bind_param("i", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
