e492677ce1d142a55c4b58bd6a2b2b145928bddf
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-13 10:15:03 -0600

    Update view-productswithstores.php

--- view-productswithstores.php
+++ view-productswithstores.php
@@ -1,4 +1,13 @@
-<h1>Products with Stores</h1>
+<div class="row">
+  <div class="col">
+  <h1>Products with Stores</h1>
+  </div>
+  <div class="col-auto">    
+    <?php
+include "view-products-newform.php";
+?>
+  </div>
+</div>
 
 <div class="card-group">
 <?php
@@ -13,7 +22,20 @@ while ($product = $products->fetch_assoc()) {
           $stores = selectStoresByProducts($product['product_id']);
           while ($store = $stores->fetch_assoc()) {
           ?>
-            <li class="list-group-item"><h5>Store Name: <?php echo $store['store_name']; ?></h5> - Date: <?php echo $store['date']; ?> Quantity: <?php echo $store['quantity']; ?> </li>
+            <li class="list-group-item"><Store Name: <?php echo $store['store_name']; ?> - Date: <?php echo $store['date']; ?> Quantity: <?php echo $store['quantity']; ?>  <?php include "view-productswithstores-editform.php"; ?>
+
+            <form method="post" action="">
+         <input type="hidden" name="pid" value="<?php echo $store['inventory_id']; ?>">
+         <input type="hidden" name="actionType" value="Delete">
+         <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+              </svg>
+         </button>
+       </form>                               
+             
+           </li>
           <?php
           }
           ?>
