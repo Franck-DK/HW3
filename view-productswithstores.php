e0d6c96a8e822818662386a477659342806d6421
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-13 00:09:56 -0600

    Update view-productswithstores.php

--- view-productswithstores.php
+++ view-productswithstores.php
@@ -1,14 +1,8 @@
-<h1>Products with Stores</h1>
 
-<div class="card-group">
-<?php
-while ($product = $products->fetch_assoc()) {
-?>
-   <div class="card">
-    <div class="card-body">
-   <div class="row">
+
+<div class="row">
   <div class="col">
-    <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
+   <h1>Products with Stores</h1>
   </div>
   <div class="col-auto">    
     <?php
@@ -16,7 +10,14 @@ include "view-stores-newform.php";
 ?>
   </div>
 </div>
-     
+
+<div class="card-group">
+<?php
+while ($product = $products->fetch_assoc()) {
+?>
+   <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
       <p class="card-text">
         <ul class="list-group">
           <?php
@@ -27,6 +28,18 @@ include "view-stores-newform.php";
             <li class="list-group-item">Store Name: <?php echo $store['store_name']; ?></li>
             <li class="list-group-item">Location: <?php echo $store['location']; ?></li>
             <li class="list-group-item">Quantity: <?php echo $store['total_quantity']; ?></li>
+           <li> <div><?php
+include "view-stores-editform.php";
+?></div><div><form method="post" action="">
+         <input type="hidden" name="sid" value="<?php echo $store['store_id']; ?>">
+         <input type="hidden" name="actionType" value="Delete">
+         <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">
+               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+              </svg>
+         </button>
+       </form></div></li>
           <?php
           }
           ?>
@@ -43,4 +56,3 @@ include "view-stores-newform.php";
 }
 ?>
 </div>
-
