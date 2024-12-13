ccbf37373180e02425db924530c101868bbf1a73
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 22:59:38 -0600

    Update model-stores.php

--- model-stores.php
+++ model-stores.php
@@ -12,4 +12,49 @@ function selectStores() {
         throw $e;
     }
 }
+
+
+function insertStores($sname, $slocation) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `stores`(`store_name`, `location`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $sname, $slocation);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateStores($sname, $slocation, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `stores` SET`store_name`= ?,`location`= ? WHERE store_id = ?");
+        $stmt->bind_param("ssi", $sname, $slocation, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteStores($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `stores` WHERE store_id = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
