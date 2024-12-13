ba6e300c65f005f89391fd27d703762276c0b7e8
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 20:48:55 -0600

    Update products.php

--- products.php
+++ products.php
@@ -14,6 +14,14 @@ if (isset($_POST['actionType'])){
       echo '<div class="alert alert-danger" role="alert">Error</div>';
     }
     break;
+
+     case "Delete":
+    if(deleteProducts($_POST['pid'])) {
+     echo '<div class="alert alert-success" role="alert">Product deleted</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
   }
 }
 $products = selectProducts();
