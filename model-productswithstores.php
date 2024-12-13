474b4fd900a773833e597f2acc677b6484f15837
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-13 09:51:07 -0600

    Update model-productswithstores.php

--- model-productswithstores.php
+++ model-productswithstores.php
@@ -41,4 +41,48 @@ WHERE
         throw $e;
     }
 }
+
+function insertInventory($pid, $sid, $date, $quantity) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare(INSERT INTO `inventory`(`product_id`, `store_id`, `date`, `quantity`) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("iisi", $pid, $sid, $date, $quantity);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateInventory($pid, $sid, $date, $quantity, $iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `inventory` SET `product_id`=?,`store_id`=?,`date`=?,`quantity`=? WHERE inventory_id=?");
+        $stmt->bind_param("iisii", $pid, $sid, $date, $quantity, $iid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteInventory($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `inventory` WHERE inventory_id = ?");
+        $stmt->bind_param("i", $iid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
