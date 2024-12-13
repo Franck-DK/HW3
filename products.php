a2406cefccdc950c0b1e47060ee1c60b14d929f1
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 20:26:12 -0600

    Update products.php

--- products.php
+++ products.php
@@ -8,7 +8,11 @@ include "view-header.php";
 if (isset($_POST['actionType'])){
   switch ($_POST['actionType']){
     case "Add":
-    insertProducts($_POST['pname'],$_POST['pdesc'],$_POST['pprice']);
+    if(insertProducts($_POST['pname'],$_POST['pdesc'],$_POST['pprice'])) {
+     echo '<div class="alert alert-success" role="alert">Product Added</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
     break;
   }
 }
