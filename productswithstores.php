c9443607c7aa9606a7b8db563d3133aabb86c8d5
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 23:54:36 -0600

    Update productswithstores.php

--- productswithstores.php
+++ productswithstores.php
@@ -4,6 +4,35 @@ require_once("model-productswithstores.php");
 
 $pageTitle= "Products With Stores";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add":
+    if(insertProductsWithStores($_POST['pname'],$_POST['sid'],$_POST['sname'],$_POST['slocation'],$_POST['squantity'])) {
+     echo '<div class="alert alert-success" role="alert">Product Added</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
+
+    case "Edit":
+    if(updateProductsWithStores($POST['pname'],$_POST['sname'],$_POST['slocation'],$_POST['squantity'],$_POST['pid'])) {
+     echo '<div class="alert alert-success" role="alert">Product Edited</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
+
+     case "Delete":
+    if(deleteProductsWithStores($_POST['pid'])) {
+     echo '<div class="alert alert-success" role="alert">Store deleted</div>';
+    } else {
+      echo '<div class="alert alert-danger" role="alert">Error</div>';
+    }
+    break;
+  }
+}
+
 $products = selectProducts();
 include "view-productswithstores.php";
 include "view-footer.php";
