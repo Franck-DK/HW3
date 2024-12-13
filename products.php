d92f2c1479a8cd9f80990cf61f8629fff4453229
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-12 20:15:23 -0600

    Update products.php

--- products.php
+++ products.php
@@ -4,6 +4,14 @@ require_once("model-products.php");
 
 $pageTitle= "Products";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch ($_POST['actionType']){
+    case "Add":
+    insertProduct($_POST['pname'],$_POST['pdesc'],$_POST['pprice']);
+    break;
+  }
+}
 $products = selectProducts();
 include "view-products.php";
 include "view-footer.php";
