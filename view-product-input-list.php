de48e953f21cce02b6042fd54ef3339a75107679
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-14 00:49:14 -0600

    Update view-product-input-list.php

--- view-product-input-list.php
+++ view-product-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="pid" name="pid">
   <?php
 while($productItem = $productList->fetch_assoc()) {
+  $selText ="";
+  if($selectedProduct == $productItem['product_id']){
+    $selText = "selected";
+  }
 ?>
-    <option value="<?php echo $productItem['product_id']; ?>"><?php echo $productItem['product_name']; ?></option>
+    <option value="<?php echo $productItem['product_id']; ?>"<?=$selText?>><?php echo $productItem['product_name']; ?></option>
 <?php  
 }
 ?>
