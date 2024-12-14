33247939cfa223f2c6807d00e3da87d06a2f40c0
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-14 01:03:01 -0600

    Update view-store-input-list.php

--- view-store-input-list.php
+++ view-store-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id="sid" name="sid">
   <?php
 while($storeItem = $storeList->fetch_assoc()) {
+  $selText ="";
+  if($selectedStore == $storeItem['store_id']){
+    $selText = "selected";
+  }
 ?>
-    <option value="<?php echo $storeItem['store_id']; ?>"><?php echo $storeItem['store_name']; ?></option>
+    <option value="<?php echo $storeItem['store_id']; ?>"<?=$selText?>><?php echo $storeItem['store_name']; ?></option>
 <?php  
 }
 ?>
