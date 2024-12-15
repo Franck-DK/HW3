832375912989e7ef2b0acfadd3514ac2097a8443
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-15 10:03:17 -0600

    Update view-products-chart.php

--- view-products-chart.php
+++ view-products-chart.php
@@ -1,1 +1,46 @@
+<h1>Products Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [
+         <?php
+                  while ($product = $products->fetch_assoc()) {
+                  echo $product['productname'].", ";
+                  }
+                  ?>
+
+      ],
+      datasets: [{
+        label: 'Total Quantity',
+        data: [
+                  <?php
+                  $products = selectProducts();
+                  while ($product = $products->fetch_assoc()) {
+                  echo "'".$product['total_quantity']."', ";
+                  }
+                  ?>
+
+          
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+ 
