529cf51fe2ed8211469234b75aa7c7b65ed3e444
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-15 21:07:01 -0600

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -1,7 +1,7 @@
 <?php
 function get_db_connection(){
     // Create connection
-    $conn = new mysqli('138.197.17.168', 'frankdko_hw3user', 'LXF38qf4.,Pm', 'frankdko_hw3');
+    $conn = new mysqli('138.197.17.168', 'frankdko_hw3user', 'LXF38qf4.,Pm', 'frankdko_hw');
     
     // Check connection
     if ($conn->connect_error) {
