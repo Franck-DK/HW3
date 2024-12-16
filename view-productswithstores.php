3eb65394653ac9256bab1b4f893f0239d8c56e4b
Franck D K <98106173+Franck-DK@users.noreply.github.com>
2024-12-15 18:16:48 -0600

    Update view-productswithstores.php

--- view-productswithstores.php
+++ view-productswithstores.php
@@ -1,62 +1,66 @@
 <div class="row">
   <div class="col">
-  <h1>Make-Models with Dealerships</h1>
+    <h1>Make-Models with Dealerships</h1>
   </div>
   <div class="col-auto">    
-    <?php
-include "view-productswithstores-newform.php";
-?>
+    <?php include "view-productswithstores-newform.php"; ?>
   </div>
 </div>
 
-<div class="card-group">
+<!-- Horizontal Cards Layout -->
+<div class="d-flex flex-wrap gap-3">
 <?php
 while ($product = $products->fetch_assoc()) {
 ?>
-   <div class="card">
-    <div class="card-body">
-      <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
-      <p class="card-text">
-        <ul class="list-group">
-          <?php
-          $stores = selectStoresByProducts($product['product_id']);
-          while ($store = $stores->fetch_assoc()) {
-          ?>
-           
-          <li class="list-group-item">
-            <div>
-              <div><h6>DealershipName: <?php echo $store['store_name']; ?></h6></div>
-              <div>Date: <?php echo $store['date']; ?></div>
-              <div>Quantity: <?php echo $store['quantity']; ?></div>
-              <div style="display: flex; flex-direction: row;">
-                <?php include "view-productswithstores-editform.php"; ?> 
-              <form method="post" action="">
-         <input type="hidden" name="iid" value="<?php echo $store['inventory_id']; ?>">
-         <input type="hidden" name="actionType" value="Delete">
-         <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">
-               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-              </svg>
-         </button>
-       </form> 
-              </div>
-              
-            </div></li>
-          <?php
-          }
-          ?>
-        </ul>
-      </p>
-      <p class="card-text">
-        <small class="text-body-secondary">
-          Description: <?php echo $product['product_description']; ?>
-        </small>
-      </p>
+  <div class="card mb-3" style="flex: 0 1 450px;"> <!-- Adjust width -->
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="https://via.placeholder.com/150" class="img-fluid rounded-start" alt="Product Image">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title"><?php echo $product['product_name']; ?></h5>
+          <p class="card-text">
+            <ul class="list-group">
+              <?php
+              $stores = selectStoresByProducts($product['product_id']);
+              while ($store = $stores->fetch_assoc()) {
+              ?>
+                <li class="list-group-item">
+                  <div>
+                    <h6>Dealership Name: <?php echo $store['store_name']; ?></h6>
+                    <div>Date: <?php echo $store['date']; ?></div>
+                    <div>Quantity: <?php echo $store['quantity']; ?></div>
+                    <div style="display: flex; flex-direction: row; gap: 10px;">
+                      <?php include "view-productswithstores-editform.php"; ?> 
+                      <form method="post" action="">
+                        <input type="hidden" name="iid" value="<?php echo $store['inventory_id']; ?>">
+                        <input type="hidden" name="actionType" value="Delete">
+                        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?')">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </li>
+              <?php } ?>
+            </ul>
+          </p>
+          <p class="card-text">
+            <small class="text-body-secondary">
+              Description: <?php echo $product['product_description']; ?>
+            </small>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
-<?php
-}
+<?php } ?>
+</div>
+
 ?>
 </div>
 
